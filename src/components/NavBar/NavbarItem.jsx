36594ac3bc244c7ad3d1f25261b3b871f7cf1790
h.habka <h.habka@staff.ososs.com>
2024-07-23 23:03:11 +0400

    fix navbar navigations

--- src/components/NavBar/NavbarItem.jsx
+++ src/components/NavBar/NavbarItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavbarItem = ({ title, href }) => {
     return (
-        <a href={href} className="hover:text-two cursor-pointer before:absolute before:w-0 before:h-[2px] before:bg-two relative before:bottom-0 before:transition-all before:duration-500 before:hover:w-full">
+        <Link href={href} to={href} className="hover:text-two cursor-pointer before:absolute before:w-0 before:h-[2px] before:bg-two relative before:bottom-0 before:transition-all before:duration-500 before:hover:w-full">
             {title}
-        </a>
+        </Link>
     );
 };
 
