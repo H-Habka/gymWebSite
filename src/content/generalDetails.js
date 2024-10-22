75589cc84f872a133eb11b6a81c4787e5ea61c02
h.habka <h.habka@staff.ososs.com>
2024-10-22 23:56:12 +0400

    fix description

--- src/content/generalDetails.js
+++ src/content/generalDetails.js
@@ -3,5 +3,5 @@ export const generalDetails = {
   jobTitle: "Naval Architect",
   descriptionTitle: "Hi, I'm Yacht and Jewellery designer",
   descriptionContent:
-    "I graduated from Tishreen University with a degree in Marine Engineering, achieving a graduation rate of 85.4%, which earned me the top position in my class. My dedication and excellence in my graduation project were recognized with an Appreciation Certificate from Mire Marine Shipping Company and Mobin Marine Design Technical Engineering.",
+    "Experienced Yacht Designer and Marine Engineer with a comprehensive background in the design, engineering and construction of marine vessels, particularly yachts. Possessing over 3 years of hands-on experience in utilizing CAD software to create functional and visually striking designs that align with client expectations and technical standards. Proficient in applying principles of naval architecture to ensure the safety, stability and optimal performance of vessels from concept to completion.",
 }
